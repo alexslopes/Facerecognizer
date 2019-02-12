dd51fc1f023854da6e80d776f832f03e5fb07912
Alex <alexstlopes@gmail.com>
2019-02-12 19:51:28 -0300

    Faces extraidas sendo salvas

--- src/main/java/com/ifba/ads/Facerecognizer/recognizer/DetectFaces.java
+++ src/main/java/com/ifba/ads/Facerecognizer/recognizer/DetectFaces.java
@@ -4,28 +4,35 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Rect;
 import org.bytedeco.javacpp.opencv_objdetect.CascadeClassifier;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
 import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
+import static org.bytedeco.javacpp.opencv_imgproc.resize;
 import static org.bytedeco.javacpp.opencv_core.RectVector;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imwrite;
 
 import com.ifba.ads.Facerecognizer.utils.FileUtils;
 
 public class DetectFaces {
 
 	static CascadeClassifier faceDetector = new CascadeClassifier("/home/alex/eclipse-workspace/Facerecognizer/cascades/frontalface.xml");
-	
+	//TODO fazer retornar a face detectada e armazenar em um array
 	public static boolean detectFaces(BufferedImage image) throws IOException {
-		//TODO converter imagens para mat
+		Rect mainFace;
 		Mat rgbaMat = FileUtils.BufferedImage2Mat(image);
-		//TODO verificar a criação do greymat na udemy
 		Mat greyMat = new Mat();
 		cvtColor(rgbaMat, greyMat, CV_BGR2GRAY);
 		RectVector faces = new RectVector();
 		faceDetector.detectMultiScale(greyMat, faces);
 		System.out.println("Faces detectadas: " + faces.size());
-		//TODO extrair faces
+		//Aqui pego apenas uma face já que o objetivo e coletar fot com apenas uma pessoa
+		mainFace = faces.get(0);
+		Mat detectFace = new Mat(greyMat, mainFace);
+		resize(detectFace, detectFace, new opencv_core.Size(160, 160));
+		imwrite("/home/alex/pessoa." + 1 + "." + 1 + ".jpg", detectFace);
 		
 		return true;
 	}
