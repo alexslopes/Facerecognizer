248926b173dfdba28aea5e39cec4e87e7f7cc0d8
Alex <alexstlopes@gmail.com>
2019-02-12 07:38:20 -0300

    Reconhecimento de faces funcionando

--- src/main/java/com/ifba/ads/Facerecognizer/recognizer/DetectFaces.java
+++ src/main/java/com/ifba/ads/Facerecognizer/recognizer/DetectFaces.java
@@ -2,15 +2,29 @@ package com.ifba.ads.Facerecognizer.recognizer;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
+import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_objdetect.CascadeClassifier;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
+import static org.bytedeco.javacpp.opencv_core.RectVector;
+
+import com.ifba.ads.Facerecognizer.utils.FileUtils;
 
 public class DetectFaces {
 
-	CascadeClassifier faceDetector = new CascadeClassifier("cascades/frontalface.xml");
+	static CascadeClassifier faceDetector = new CascadeClassifier("/home/alex/eclipse-workspace/Facerecognizer/cascades/frontalface.xml");
 	
-	public boolean detectFaces(BufferedImage image) {
+	public static boolean detectFaces(BufferedImage image) throws IOException {
 		//TODO converter imagens para mat
+		Mat rgbaMat = FileUtils.BufferedImage2Mat(image);
+		//TODO verificar a criação do greymat na udemy
+		Mat greyMat = new Mat();
+		cvtColor(rgbaMat, greyMat, CV_BGR2GRAY);
+		RectVector faces = new RectVector();
+		faceDetector.detectMultiScale(greyMat, faces);
+		System.out.println("Faces detectadas: " + faces.size());
 		//TODO extrair faces
 		
 		return true;
--- src/main/java/com/ifba/ads/Facerecognizer/services/Service.java
+++ src/main/java/com/ifba/ads/Facerecognizer/services/Service.java
@@ -1,20 +1,23 @@
 package com.ifba.ads.Facerecognizer.services;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.imageio.ImageIO;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.ifba.ads.Facerecognizer.recognizer.DetectFaces;
 import com.ifba.ads.Facerecognizer.utils.FileUtils;
-//import com.ifba.ads.Facerecognizer.utils.FileUtils;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 
-@Path("/service")
+@Path("/upload")
 public class Service {
 	
 	private static final String UPLOAD_FOLDER = "/home/alex/";
@@ -43,6 +46,15 @@ public class Service {
 			FileUtils.saveToFile(uploadedInputStream, uploadedFileLocation);
 		} catch (IOException e) {
 			return Response.status(500).entity("Can not save file").build();
+		}
+		
+		try {
+			System.out.println(uploadedFileLocation);
+			BufferedImage image = ImageIO.read(new File(uploadedFileLocation));
+			DetectFaces.detectFaces(image);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		return Response.status(200).entity("File saved to " + uploadedFileLocation).build();
